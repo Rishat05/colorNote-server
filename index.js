998e773d41acd204152f84fedfaabeb79418aba3
rishat <rishatrifa56@gmail.com>
2022-09-23 20:28:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zgbvl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const notesCollection = client.db('colorNote').collection('userNote'); //colorNote.userNote
+
+        app.get('/note', async (req, res) => {
+            const query = req.query;
+            const cursor = notesCollection.find(query);
+            const notes = await cursor.toArray();
+            res.send(notes);
+        });
+
+        app.post('/note', async (req, res) => {
+            const query = req.body;
+            const result = await notesCollection.insertOne(query);
+            res.send(result);
+        });
+
+        app.put("/note/:id", async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: {
+                    text: data.text,
+                },
+            };
+
+            const result = await notesCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+        app.delete("/note/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await notesCollection.deleteOne(filter);
+            res.send(result);
+        });
+    }
+    catch {
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('color notes');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
